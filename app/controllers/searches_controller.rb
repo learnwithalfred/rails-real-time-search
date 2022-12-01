030b7b29d9abc175cbc327d5e6539d27a3f24388
Alfred Boateng <programmingwithalfred@gmail.com>
2022-12-01 14:32:40 +0000

    Finish up code
    Add styles and navbar

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -9,6 +9,6 @@ class SearchesController < ApplicationController
     # group searches by query and count them
     @limit = 100
     @time_range = 1.weeks.ago..Time.now
-    @searches = Search.where(created_at: @time_range).group(:query).limit(20).order('query asc').limit(@limit).count
+    @searches = Search.where(created_at: @time_range).where(ip_address: request.remote_ip).group(:query).limit(20).order('query asc').limit(@limit).count
   end
 end
